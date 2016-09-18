955aac8c4c35d4e370c36d1b26f640d0940ca94a
Brandon Peck <brandonpeck@d-172-25-98-195.dhcp.virginia.edu>
2016-09-18 15:04:44 -0400

    removed migrations

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,17 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/user/(?P<user_id>[0-9]+)/$', views.getUser, name='getUser'),
-<<<<<<< HEAD
     url(r'^api/user/n/$', views.createUser, name='createUser'),
     url(r'^api/user/all/$', views.getAllUsers, name='getAllUsers'),
     url(r'^api/job/(?P<job_id>[0-9]+)/$', views.getJob, name='getJob'),
     url(r'^api/job/n/$', views.createJob, name='createJob'),
-    url(r'^api/job/all/$', view.getAllJobs, name='getAllJobs'),
-
-
-=======
-    url(r'^api/user/', views.getAllUsers, name='getAllUsers'),
-    url(r'^api/job/', views.getAllJobs, name='getAllJobs'),
-    url(r'^api/job/(?P<job_id>[0-9]+)/$', views.getJob, name='getJob'),
->>>>>>> master
+    url(r'^api/job/all/$', views.getAllJobs, name='getAllJobs'),
 ]
